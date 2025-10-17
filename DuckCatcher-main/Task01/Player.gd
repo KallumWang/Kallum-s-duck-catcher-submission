723707787a1d1a1126658387f69408f21677b345
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-10-17 14:21:38 +1300

    extra fixes

--- DuckCatcher-main/Task01/Player.gd
+++ DuckCatcher-main/Task01/Player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 @onready var sprite = $Sprite2D
 var speed = 670
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	# Move left/right
 	var direction = Input.get_axis("ui_left", "ui_right")
 	velocity.x = direction * speed
